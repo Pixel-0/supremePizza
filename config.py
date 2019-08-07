3e59b680cba1af9adb715e1ae0fc17837eeb6846
Pixel-0 <tiny0pixel@gmail.com>
2019-08-07 17:23:04 +0300

    models.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:crazy@localhost/pizza'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS= True
@@ -10,13 +11,17 @@ class Config:
         pass
 
 class ProdConfig(Config):
-
+    
     pass
 
 class  DevConfig(Config):
+    
+
     DEBUG = True
 
 config_options = {
     'development': DevConfig,
     'production':ProdConfig
 }
+
+
